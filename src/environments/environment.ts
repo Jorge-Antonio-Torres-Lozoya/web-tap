89131333903f362b09099ef8f00c99ba6a84048c
Jorge-Antonio-Torres-Lozoya <jtorreslozoya02@gmail.com>
2026-06-06 12:13:40 -0600

    fix(routing): send unauthenticated users to /login from root

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ import { Environment } from './environment.model';
 // Production environment
 export const environment: Environment = {
   production: true,
-  apiBaseUrl: 'https://web-tap-production.up.railway.app/api/v1',
+  apiBaseUrl: 'https://api-tap-production.up.railway.app/api/v1',
 };
